5228dd91947dae9e46d7094773fa9a61132941ed
James J Balamuta <james.balamuta@gmail.com>
2016-04-07 16:00:56 -0500

    - Bumped package version to 0.2.0 for cvfold fix. - Added package help file info.

---
+++ R/panning-package.r
@@ -0,0 +1,27 @@
+#' An implementation of the Panning Algorithm
+#'
+#' Functions to run the Panning Algorithm in R.
+#'
+#' @details
+#' \tabular{ll}{
+#' Package: \tab panning\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.2.0\cr
+#' Date: \tab 2016-04-07\cr
+#' License: \tab GPL 2\cr
+#' }
+#'
+#' @author
+#' Samuel Orso \email{Samuel.Orso@@unige.ch},
+#' Roberto Molinari \email{roberto.molinari@@unige.ch},
+#' Stephane Guerrier \email{stephane@@illinois.edu},
+#' James Balamuta \email{balamut2@@illinois.edu},
+#' Nabil Mili,
+#' Marco Avella-Medina,
+#' Yanyuan Ma
+#'
+#' Stephane Guerrier \email{stephane@@illinois.edu}
+#' @name panning
+#' @docType package
+#' @exportPattern ^[[:alpha:]]+
+NULL
